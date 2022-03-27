45c2250b3a31c5efce4eba221794bf7f460327ee
zellko <wyttenda@tutanota.com>
2022-03-27 20:26:03 +0200

    Added password format / password match check

--- js/script.js
+++ js/script.js
@@ -2,23 +2,34 @@ const passwordInput = document.querySelector("#password");
 const passwordConfirmInput = document.querySelector("#passwordConfirm");
 const pwCheckList = document.querySelectorAll("li");
 
-
-console.log(passwordInput);
-
-function checkPassword(e) {
-    console.log("____");
-    console.log(e);
-    console.log(this.value);
-    console.log(pwCheckList);
-    const passwordInputValue = this.value;
-
+function checkPassword() {
     // Check if password length is valid
-    (passwordInputValue.length >= 6) ? pwCheckList[0].classList.add("valid"): pwCheckList[0].classList.remove("valid");
+    (passwordInput.value.length >= 6) ? pwCheckList[0].classList.add("valid"): pwCheckList[0].classList.remove("valid");
     // Check if password contain at least a number
-    (passwordInputValue.match(/\d/)) ? pwCheckList[1].classList.add("valid"): pwCheckList[1].classList.remove("valid");
+    (passwordInput.value.match(/\d/)) ? pwCheckList[1].classList.add("valid"): pwCheckList[1].classList.remove("valid");
     // Check if password contain at least one uppercase
-    (passwordInputValue.match(/[A-Z]/)) ? pwCheckList[2].classList.add("valid"): pwCheckList[2].classList.remove("valid");
+    (passwordInput.value.match(/[A-Z]/)) ? pwCheckList[2].classList.add("valid"): pwCheckList[2].classList.remove("valid");
+
+    checkMatchingPassword();
+
+};
+
+
+function checkMatchingPassword() {
+
+    //console.log(passwordInput.value, passwordConfirmInput.value);
+
+    // Check if both password and confirm password fields matches
+    if (passwordInput.value === passwordConfirmInput.value) {
+        passwordConfirmInput.setCustomValidity('');
+        pwCheckList[3].classList.add("valid");
+        return;
+    };
+
+    passwordConfirmInput.setCustomValidity("Passwords Don't Match");
+    pwCheckList[3].classList.remove("valid");
 
 };
 
 passwordInput.addEventListener("keyup", checkPassword);
+passwordConfirmInput.addEventListener("keyup", checkMatchingPassword);
